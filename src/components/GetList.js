7d6f296c1544d8c5c78c0cd773c05db857aa7689
testerLMJ <78alswo79@gmail.com>
2023-08-30 12:02:21 +0900

    리액트&스프링부트 삭제&업데이트 기능 구현

--- src/components/GetList.js
+++ src/components/GetList.js
@@ -1,16 +1,18 @@
 import {Link} from "react-router-dom";
 
-function GetList({id, text}) {
+function GetList({id, text, parentFn}) {
 
     return (
+    <>
         <div>
             <div>
                 <Link to={`/detail/${id}`}><h3>{id}바로가기</h3></Link> 
                 <ul>
-                    {text}
+                <input type="checkbox" value={id} onChange={event => parentFn(event)}/>{text}
                 </ul>
             </div>
         </div>
+    </>
     )
 
 }
--- src/route/Home.js
+++ src/route/Home.js
@@ -1,20 +1,72 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import GetList from "../components/GetList";
+import qs from "qs";
 
 function Home() {
     const [test, setTest] = useState([]);
     const [loading, setLoading] = useState(true);
 
+    // 서버에 보내줄 데이터 [] 값 
+    const [arr2, setArr2] = useState([]);
+    let sendData = {};
+
     useEffect(() => {
+        getSelectList();
+    }, []);
+
+    // 자식 컴포넌트에서 이벤트 발생!
+    const parentFn = (event) => {
+        if (event.target.checked) {
+            setArr2(current => [event.target.value, ...current]);
+        } else {
+            setArr2(() => {
+                return arr2.filter(item => item !== event.target.value)
+            })
+        }
+    }
+
+    const submitFn = (gbn) => {
+        const headers = { 'Content-Type' : 'application/json'};
+        // axios 쿼리 파리미터 stringify 처리! 
+        axios.defaults.paramsSerializer = params => {
+            return qs.stringify(params);
+        }
+
+        if (gbn === 'update') {
+            axios.get(`/api/${gbn}`, {
+                params: {
+                    "id": JSON.stringify(sendData.id)
+                    , "text": JSON.stringify(sendData.text)
+                },
+            }, {headers})
+            .then(() => getSelectList())
+            .catch(err => console.log(err));
+
+        } else if (gbn === 'delete') {
+            axios.get(`/api/${gbn}`, {
+                params : {
+                    "id": JSON.stringify(sendData.id)
+                }
+            }, { headers })
+            .then(res => getSelectList())
+            .catch(err => console.log(err));
+        }
+    }
+
+    // SelectList가져오는 함수
+    const getSelectList = () => {
         axios.get('/api/selectTest')
         .then(response => setTest(response.data))
         .catch(err => console.log("err!!!", err));
 
         setLoading(false);
-    }, [])
+    }
 
-    console.log(test)
+    sendData = {
+       id : arr2
+       , text : '나 여기서 두 팔 벌려 힘껏 달려와'
+    };
 
     return (
         <>
@@ -24,12 +76,16 @@ function Home() {
                 ) : (
                     <div>
                         <h1>React & Springboot 테스팅!!</h1>
+                        <button onClick={e => submitFn("update")}>수정</button>
+                        <button onClick={e => submitFn("delete")}>삭제</button>
                         <hr />
                         {test.map((item) => 
                             <GetList 
                                 key={item.id}
                                 id={item.id}
                                 text={item.text}
+                                // 자식 컴포넌트 요소로 함수를 넘겨준다.
+                                parentFn={parentFn}
                             />
                         )}
                     </div>
